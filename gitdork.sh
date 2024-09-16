4643e0df10705739c2b15a332b2b3ad1825c8db9
PentestPuppy <170636746+PentestPuppy@users.noreply.github.com>
2024-09-16 15:51:20 -0400

    Add ansi color

--- gitdork.sh
+++ gitdork.sh
@@ -5,7 +5,7 @@
 
 # By TrashPuppy 2024
 
-# USE: bash gitdork.sh <domain name> 
+# USE: bash gitdork.sh <domain name>
 # OR : bash gitdork.sh then enter your domain name into the prompt
 
 echo ''
@@ -23,13 +23,13 @@ fi
 # Build URL query based on regex:
 if [[ "$input" =~ ([\.]) ]]; then
 
-	IFS='.' read period_replaced_by_space_fuq_u_bash <<< $input			
-	IFS=' ' read domain tld <<< $period_replaced_by_space_fuq_u_bash		
+	IFS='.' read period_replaced_by_space_fuq_u_bash <<< $input
+	IFS=' ' read domain tld <<< $period_replaced_by_space_fuq_u_bash
 
 	# Check if they want the entire domain in the query:
 	echo ":-- Do you want to dork the entire domain name? Ex: ('target.com'): y/n"
 	read -a tldin
-	
+
 	if [[ "$tldin" = 'y' ]]; then
 		domain_string=$(echo "/\b$domain\\.$tld/")
 #		echo $domain_string
@@ -39,7 +39,7 @@ if [[ "$input" =~ ([\.]) ]]; then
 	fi
 else
 	domain_string=$(echo "/\b$input/")
-fi	
+fi
 
 # Rest of the URL:
 url_start='https://github.com/search?q='
@@ -61,7 +61,7 @@ read -r -d '' PAR <<- EOM
 	beanstalk.yml
 	client_secret
 	config
-	composer.json	
+	composer.json
 	credentials
 	db
 	deploy.rake
@@ -89,8 +89,8 @@ read -r -d '' PAR <<- EOM
 EOM
 
 echo ""
-echo "Just the domain :-- $url_start$domain_string$url_end"
+echo -e "\033[32mJust the domain\033[0m :-- $url_start$domain_string$url_end"
 
 for line in $PAR; do
-	echo "$line :-- $url_start$domain_string+$line$url_end"
+	echo -e "\033[32m$line\033[0m :-- $url_start$domain_string+$line$url_end"
 done
